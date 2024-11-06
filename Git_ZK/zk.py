5f698bf0c4f4733f43513bdd05f28c49b78f4d90
Kadlecik <ondrej.kadlecik@seznam.cz>
2024-11-06 21:06:12 +0100

    ok

---
+++ Git_ZK/zk.py
@@ -0,0 +1,12 @@
+text_data = "ahoj Kámo"
+
+file = open("test1.txt", "w")
+file.write(text_data)
+file.close()
+
+
+file = open("test1 .txt", "r")
+file_data = file.read()
+file.close()
+
+print(file_data)
